ffafd9f9dc3eab38ef11537edc8cef3a25c2b24a
3tjwls7 <seojin53185318@gmail.com>
2026-05-28 00:20:52 +0900

    refactor: 관리자 기능 url 수정

--- backend/src/main/java/com/safesign/backend/domain/admin/controller/AdminAnalysisController.java
+++ backend/src/main/java/com/safesign/backend/domain/admin/controller/AdminAnalysisController.java
@@ -18,7 +18,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/admin/analysis")
+@RequestMapping("/api/v1/admin/analysis")
 @RequiredArgsConstructor
 public class AdminAnalysisController {
 
